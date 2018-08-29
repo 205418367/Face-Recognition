8cba0b13114fba8c7fa6d2a8a641229e6b55fef2
超人 <38721656+394781865@users.noreply.github.com>
2018-08-29 15:18:31 +0800

    Update face_model.py

--- vedio_classification/face_model.py
+++ vedio_classification/face_model.py
@@ -11,7 +11,6 @@ import random
 import cv2
 import sklearn
 from sklearn.decomposition import PCA
-from time import 
 from easydict import EasyDict as edict
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'src', 'common'))
 import face_preprocess
